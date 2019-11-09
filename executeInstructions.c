d6dc5e0ade532e20bc8161cf8d66b8f2bacc4942
Luke <lthunga@iastate.edu>
2019-11-09 23:09:24 +0000

    5:09

--- executeInstructions.c
+++ executeInstructions.c
@@ -766,31 +766,22 @@ int shiftStages(struct machineState* machineState)
       machineState->stages[1] = empty;
     }
   }
+  if(machineState->stages[0].format==5 || machineState->stages[1].format==5 || machineState->stages[2].format==5 || machineState->stages[3].format==5 || machineState->stages[4].format==5)
+  {
+    shiftStages(machineState);
+  }
   if(machineState->stages[1].type==0)
   {
-    if(machineState->stages[0].type!=0 && machineState->stages[2].format!=5 && machineState->stages[3].format!=5 && machineState->stages[4].format!=5)
+    if(machineState->stages[0].type!=0)
     {
       machineState->stages[1] = machineState->stages[0];
       machineState->stages[0] = empty;
     }
-    else if(machineState->count!=machineState->length && machineState->count!=-1 && machineState->stages[2].format!=5 && machineState->stages[3].format!=5 && machineState->stages[4].format!=5)
+    else if(machineState->count!=machineState->length && machineState->count!=-1)
     {
       machineState->stages[1] = machineState->instruction;
       machineState->unpipelinedCycles++;
       return 1;
-    }
-    if(finished.format==5)
-    {
-      return 1;
-    }
-    if(machineState->stages[2].format==5 || machineState->stages[3].format==5 || machineState->stages[4].format==5)
-    {
-      int i;
-      for(i=0; i<4; i++)
-      {
-        shiftStages(machineState);
-      }
-      machineState->controlHazards++;
     }
   }
   if(machineState->count!=machineState->length && machineState->count!=-1 && machineState->stages[0].type==0)
