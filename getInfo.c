06d986b9a4fae009d3a7dd4ca54a5aa8cdf39dee
Luke <lthunga@iastate.edu>
2019-11-10 23:01:15 +0000

    5:02

--- getInfo.c
+++ getInfo.c
@@ -383,7 +383,6 @@ void setVariables(int format, uint32_t newInstruction, struct instruction* progr
   {
     mask = ((1 << 26) - 1) << 0;
     shifted = newInstruction & mask;
-    program[j].c1 = shifted;
     uint32_t temp = shifted >> 25;
     if(temp == 1)
     {
